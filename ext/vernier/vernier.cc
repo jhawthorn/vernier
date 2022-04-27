2463c091b5e4ce8d1a1772b0f67c7ab800b44aa5
John Hawthorn <john@hawthorn.email>
2022-04-26 22:29:23 -0700

    Report the type in the flamegraph

--- ext/vernier/vernier.cc
+++ ext/vernier/vernier.cc
@@ -88,6 +88,49 @@ trace_retained_start(VALUE self) {
 // HACK: This isn't public, but the objspace ext uses it
 extern "C" size_t rb_obj_memsize_of(VALUE);
 
+static const char *
+ruby_object_type_name(VALUE obj) {
+    enum ruby_value_type type = rb_type(obj);
+
+#define TYPE_CASE(x) case (x): return (#x)
+
+    // Many of these are impossible, but it's easier to just include them
+    switch (type) {
+        TYPE_CASE(T_OBJECT);
+        TYPE_CASE(T_CLASS);
+        TYPE_CASE(T_MODULE);
+        TYPE_CASE(T_FLOAT);
+        TYPE_CASE(T_STRING);
+        TYPE_CASE(T_REGEXP);
+        TYPE_CASE(T_ARRAY);
+        TYPE_CASE(T_HASH);
+        TYPE_CASE(T_STRUCT);
+        TYPE_CASE(T_BIGNUM);
+        TYPE_CASE(T_FILE);
+        TYPE_CASE(T_DATA);
+        TYPE_CASE(T_MATCH);
+        TYPE_CASE(T_COMPLEX);
+        TYPE_CASE(T_RATIONAL);
+
+        TYPE_CASE(T_NIL);
+        TYPE_CASE(T_TRUE);
+        TYPE_CASE(T_FALSE);
+        TYPE_CASE(T_SYMBOL);
+        TYPE_CASE(T_FIXNUM);
+        TYPE_CASE(T_UNDEF);
+
+        TYPE_CASE(T_IMEMO);
+        TYPE_CASE(T_NODE);
+        TYPE_CASE(T_ICLASS);
+        TYPE_CASE(T_ZOMBIE);
+        TYPE_CASE(T_MOVED);
+
+        default:
+        return "unknown type";
+    }
+#undef TYPE_CASE
+}
+
 static VALUE
 trace_retained_stop(VALUE self) {
     rb_tracepoint_disable(tp_newobj);
@@ -106,6 +149,7 @@ trace_retained_stop(VALUE self) {
             ss << frame;
             if (i > 0) ss << ";";
         }
+        ss << ";" << ruby_object_type_name(obj);
         ss << " " << rb_obj_memsize_of(obj) << endl;
     }
 
