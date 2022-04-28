1624b4b14d68d16c6a7db67dd319d32786299781
John Hawthorn <john@hawthorn.email>
2022-04-27 21:12:50 -0700

    Fix tests somewhat

--- test/test_vernier.rb
+++ test/test_vernier.rb
@@ -1,6 +1,29 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "json"
+
+class ReportReader
+  def initialize(json)
+    @data = JSON.parse(json)
+  end
+
+  def profile_hash
+    @profile_hash ||= @data["profiles"][0]
+  end
+
+  def frames_array
+    @frames_array ||= @data["shared"]["frames"]
+  end
+
+  def weights
+    profile_hash["weights"]
+  end
+
+  def total_bytes
+    weights.sum
+  end
+end
 
 class TestVernier < Minitest::Test
   def test_that_it_has_a_version_number
@@ -19,17 +42,17 @@ class TestVernier < Minitest::Test
       }
     end
 
-    lines = result.lines.map(&:chomp)
+    result = ReportReader.new(result)
 
-    assert lines.size < 200
-    assert lines.size > 100
+    assert result.total_bytes > 40 * 100
+    assert result.total_bytes < 40 * 200
 
-    lines_matching_retained = lines.select do |line|
-      line.include?("#{__FILE__}:#{start_line+5}")
-    end
+    #lines_matching_retained = lines.select do |line|
+    #  line.include?("#{__FILE__}:#{start_line+5}")
+    #end
 
-    # WHY 101 and not 100 ????
-    assert lines_matching_retained.size == 101
+    ## WHY 101 and not 100 ????
+    #assert lines_matching_retained.size == 101
 
     #lines.uniq.each do |line|
     #  puts "="*80
