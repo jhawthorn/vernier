2dbf20d0c5f2658f07f8b1d91f4049f483368872
John Hawthorn <john@hawthorn.email>
2023-07-20 01:54:52 -0700

    Add example http script

---
+++ examples/threaded_http_requests.rb
@@ -0,0 +1,38 @@
+require "vernier"
+
+Vernier.trace(out: "http_requests.json") do
+
+require "net/http"
+require "uri"
+require "openssl"
+
+urls = Queue.new
+received = Queue.new
+
+threads = 2.times.map do
+  Thread.new do
+    while url = urls.pop
+      uri = URI.parse(url)
+      response = Net::HTTP.get_response(uri)
+
+      received << [url, response.code]
+    end
+  end
+end
+
+Thread.new do
+  threads.each(&:join)
+  received.close
+end
+
+urls << "http://example.com"
+urls << "https://www.johnhawthorn.com"
+urls << "https://tenderlovemaking.com/"
+urls.close
+
+while x = received.pop
+  url, code = *x
+  puts "#{url} #{code}"
+end
+
+end
