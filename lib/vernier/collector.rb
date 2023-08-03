33479677cd09075a8b8a7f8879683d70b518897c
Aaron Patterson <tenderlove@ruby-lang.org>
2023-08-02 22:42:03 -0400

    testing custom markers

--- lib/vernier/collector.rb
+++ lib/vernier/collector.rb
@@ -6,6 +6,30 @@ module Vernier
   class Collector
     def initialize(mode)
       @mode = mode
+      @markers = []
+    end
+
+    ##
+    # Get the current time.
+    #
+    # This method returns the current time from Process.clock_gettime in
+    # integer nanoseconds.  It's the same time used by Vernier internals and
+    # can be used to generate timestamps for custom markers.
+    def current_time
+      Process.clock_gettime(Process::CLOCK_MONOTONIC, :nanosecond)
+    end
+
+    ##
+    # Record an interval with a name.  Yields to a block and records the amount
+    # of time spent in the block as an interval marker.
+    def record_interval name
+      start = current_time
+      yield
+      @markers << [Thread.current.native_thread_id,
+                   name,
+                   start,
+                   current_time,
+                   Marker::Phase::INTERVAL]
     end
 
     def stop
@@ -44,6 +68,8 @@ module Vernier
           markers << [tid, name, sym, ts, finish, phase]
         end
       end
+
+      markers.concat @markers
 
       result.instance_variable_set(:@markers, markers)
 
