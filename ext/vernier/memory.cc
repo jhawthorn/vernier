ff2c002f6e1ae359abf6594d84f353ae15b3600b
John Hawthorn <john@hawthorn.email>
2024-07-15 01:51:39 -0700

    Running thread to collect memory

--- ext/vernier/memory.cc
+++ ext/vernier/memory.cc
@@ -8,6 +8,7 @@
 #include <unistd.h>
 
 #include "vernier.hh"
+#include "timestamp.hh"
 
 inline uint64_t memory_rss() {
     pid_t pid = getpid();
@@ -40,21 +41,78 @@ static VALUE rb_memory_rss(VALUE self) {
     return ULL2NUM(memory_rss());
 }
 
-class MemoryTracker {
+class PeriodicThread {
+    std::atomic<bool> running;
+    pthread_t pthread;
+    TimeStamp interval;
+
     public:
-        struct Record {
-            uint64_t memory_rss;
-        };
-        std::vector<Record> results;
+        PeriodicThread() : interval(TimeStamp::from_milliseconds(10)) {
+        }
+
+        void set_interval(TimeStamp timestamp) {
+            interval = timestamp;
+        }
+
+        static void *thread_entrypoint(void *arg) {
+            static_cast<PeriodicThread *>(arg)->run();
+            return NULL;
+        }
+
+        void run() {
+            TimeStamp next_sample_schedule = TimeStamp::Now();
+            while (running) {
+                TimeStamp sample_complete = TimeStamp::Now();
+
+                run_iteration();
+
+                next_sample_schedule += interval;
+
+                if (next_sample_schedule < sample_complete) {
+                    next_sample_schedule = sample_complete + interval;
+                }
+
+                TimeStamp::SleepUntil(next_sample_schedule);
+            }
+        }
+
+        virtual void run_iteration() = 0;
 
         void start() {
+            if (running) return;
+
+            running = true;
+
+            int ret = pthread_create(&pthread, NULL, &thread_entrypoint, this);
+            if (ret != 0) {
+                perror("pthread_create");
+                rb_bug("VERNIER: pthread_create failed");
+            }
         }
 
         void stop() {
+            if (!running) return;
+
+            running = false;
+        }
+};
+
+class MemoryTracker : public PeriodicThread {
+    public:
+        struct Record {
+            TimeStamp timestamp;
+            uint64_t memory_rss;
+        };
+        std::vector<Record> results;
+        std::mutex mutex;
+
+        void run_iteration() {
+            record();
         }
 
         void record() {
-            results.push_back(Record{memory_rss()});
+            const std::lock_guard<std::mutex> lock(mutex);
+            results.push_back(Record{TimeStamp::Now(), memory_rss()});
         }
 };
 
@@ -92,11 +150,13 @@ VALUE memory_tracker_record(VALUE self) {
 VALUE memory_tracker_results(VALUE self) {
     MemoryTracker *memory_tracker;
     TypedData_Get_Struct(self, MemoryTracker, &rb_memory_tracker_type, memory_tracker);
-    VALUE arr = rb_ary_new();
+    VALUE timestamps = rb_ary_new();
+    VALUE memory = rb_ary_new();
     for (const auto& record: memory_tracker->results) {
-        rb_ary_push(arr, ULL2NUM(record.memory_rss));
+        rb_ary_push(timestamps, ULL2NUM(record.timestamp.nanoseconds()));
+        rb_ary_push(memory, ULL2NUM(record.memory_rss));
     }
-    return arr;
+    return rb_ary_new_from_args(2, timestamps, memory);
 }
 
 VALUE memory_tracker_alloc(VALUE self) {
--- test/test_memory_tracker.rb
+++ test/test_memory_tracker.rb
@@ -3,13 +3,23 @@
 require "test_helper"
 
 class TestMemoryTracker < Minitest::Test
-  def test_memory_rss
+  def test_explicit_record
     memory_tracker = Vernier::MemoryTracker.new
     memory_tracker.record
     memory_tracker.record
     memory_tracker.record
 
-    results = memory_tracker.results
-    assert_equal 3, results.size
+    timestamps, memory = memory_tracker.results
+    assert_equal 3, timestamps.size
+    assert_equal timestamps.size, memory.size
+  end
+
+  def test_start_and_stop
+    memory_tracker = Vernier::MemoryTracker.new
+    memory_tracker.start
+    sleep 0.2
+    timestamps, memory = memory_tracker.results
+    assert_includes (18..22), timestamps.size
+    assert_equal timestamps.size, memory.size
   end
 end
