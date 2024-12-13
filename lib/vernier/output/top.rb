0a48c8dba096ce10a2a4862e1c5ff6ed83946007
John Hawthorn <john@hawthorn.email>
2024-12-12 22:20:31 -0800

    Table formatting for top

--- lib/vernier/output/top.rb
+++ lib/vernier/output/top.rb
@@ -7,6 +7,45 @@ module Vernier
         @profile = profile
       end
 
+      class Table
+        def initialize(header)
+          @header = header
+          @rows = []
+          yield self
+        end
+
+        def <<(row)
+          @rows << row
+        end
+
+        def to_s
+          (
+            [
+              row_separator,
+              format_row(@header),
+              row_separator
+            ] + @rows.map do |row|
+              format_row(row)
+            end + [row_separator]
+          ).join("\n")
+        end
+
+        def widths
+          @widths ||=
+            (@rows + [@header]).transpose.map do |col|
+              col.map(&:size).max
+            end
+        end
+
+        def row_separator
+          @row_separator = "+" + widths.map { |i| "-" * (i + 2) }.join("+") + "+"
+        end
+
+        def format_row(row)
+          "|" + row.map.with_index { |str, i| " " + str.ljust(widths[i] + 1) }.join("|") + "|"
+        end
+      end
+
       def output
         thread = @profile.main_thread
         stack_table =
@@ -21,6 +60,8 @@ module Vernier
           stack_weights[stack_idx] += weight
         end
 
+        total = stack_weights.values.sum
+
         top_by_self = Hash.new(0)
         stack_weights.each do |stack_idx, weight|
           frame_idx = stack_table.stack_frame_idx(stack_idx)
@@ -34,6 +75,12 @@ module Vernier
           s << "#{samples}\t#{frame}\n"
         end
         s
+        Table.new %w[Samples % name] do |t|
+          top_by_self.sort_by(&:last).reverse.each do |frame, samples|
+            pct = 100.0 * samples / total
+            t << [samples.to_s, pct.round(1).to_s, frame]
+          end
+        end.to_s
       end
     end
   end
