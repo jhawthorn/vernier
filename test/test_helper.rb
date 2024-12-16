0facea3fe9da171c45aa75ebab01672285c06a71
John Hawthorn <john@hawthorn.email>
2024-12-16 00:07:44 -0800

    Add more assertions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,10 +20,36 @@ class Minitest::Test
 
     stack_table_size = result._stack_table.stack_count
 
-    result.samples.each do |stack_idx|
-      assert_kind_of Integer, stack_idx
-      assert_operator stack_idx, :<, stack_table_size
-      assert_operator stack_idx, :>=, 0
+    assert_kind_of Integer, result.pid
+    assert_kind_of Integer, result.end_time
+    assert_kind_of Integer, result.started_at
+
+    meta = result.meta
+    assert_kind_of Hash, meta
+    mode = meta[:mode]
+    assert_kind_of Symbol, mode
+
+    threads = result.threads
+    assert_kind_of Hash, threads
+    refute_empty threads
+
+    threads.each do |tid, thread|
+      assert_kind_of Integer, thread[:tid]
+      assert_kind_of String, thread[:name]
+      assert_kind_of Integer, thread[:started_at]
+
+      assert_kind_of Array, thread[:samples]
+      assert_kind_of Array, thread[:weights]
+      unless mode == :retained
+        assert_kind_of Array, thread[:timestamps]
+        assert_kind_of Array, thread[:sample_categories]
+      end
+
+      thread[:samples].each do |stack_idx|
+        assert_kind_of Integer, stack_idx
+        assert_operator stack_idx, :<, stack_table_size
+        assert_operator stack_idx, :>=, 0
+      end
     end
   end
 end
