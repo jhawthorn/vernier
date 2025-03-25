37328452b02af1405bc349fe71dc4fd43783236b
Dale Hamel <dale.hamel@shopify.com>
2025-03-25 12:16:25 -0400

    Add integration test for vernier run metadata

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -12,6 +12,43 @@ class IntegrationTest < Minitest::Test
     assert_valid_firefox_profile(result)
   end
 
+  def test_vernier_run
+    result = vernier_run("--", "ruby", "-e", "'sleep 1'")
+    assert_valid_firefox_profile(result)
+  end
+
+  def test_vernier_run_with_metadata
+    result = vernier_run("--metadata", "key1=val1",
+                         "--metadata", "key2=val2",
+                         "--", "ruby", "-e", "'sleep 1'")
+    assert_valid_firefox_profile(result)
+    assert_equal result["meta"]["userMetadata"]["key1"], "val1"
+    assert_equal result["meta"]["userMetadata"]["key2"], "val2"
+  end
+
+  def test_vernier_run_with_metadata_last_value_takes_precedence
+    result = vernier_run("--metadata", "key1=val1",
+                         "--metadata", "key2=val2",
+                         "--metadata", "key1=val3",
+                         "--", "ruby", "-e", "'sleep 1'")
+    assert_valid_firefox_profile(result)
+    assert_equal result["meta"]["userMetadata"]["key2"], "val2"
+    assert_equal result["meta"]["userMetadata"]["key1"], "val3"
+  end
+
+  # Explicitly call the "vernier run" executable
+  def vernier_run(*argv)
+    result_json = nil
+    Tempfile.open('vernier') do |tempfile|
+      result = system('vernier', 'run', '--output', tempfile.path, *argv)
+      tempfile.rewind
+      result_json = tempfile.read
+    end
+    result = JSON.parse(result_json)
+    result
+  end
+
+  # Directly call autoload, mimicking the behaviour of vernier run
   def run_vernier(options, *argv)
     result_json = nil
     Tempfile.open('vernier') do |tempfile|
